63766c703b905311376b96431e2e5ce56a4f3391
kaji-hiro <kajihiro190206@gmail.com>
2019-11-13 14:21:15 +0900

    create session

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  # static_pages
   root 'static_pages#home'
   get 'static_pages/about'
   get 'static_pages/contact'
+  # sessions
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+  # users
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
