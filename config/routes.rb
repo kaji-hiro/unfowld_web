bce77c7301d42d37f747dad862925ad46fc6a338
kaji-hiro <kajihiro190206@gmail.com>
2019-11-13 14:25:55 +0900

    fix ruby version

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,4 @@ Rails.application.routes.draw do
   get 'static_pages/contact'
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end 
+end
