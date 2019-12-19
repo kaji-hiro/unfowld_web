a84e5069f5f10446bde6b0594d9b6e649ff3dfb4
kaji-hiro <kajihiro190206@gmail.com>
2019-12-19 16:52:17 +0900

    add google track

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,30 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+        title: 'Unfowld',
+        description: '明治大学ファッションサークルUnfowldのホームページです',
+        keywords: 'Unfowld,unfowld,明治大学,ファッション,fashion,サークル,明治',
+        icon: [
+            {href: image_tag('/logo.png')},
+            {href: image_tag('/logo.png'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/jpg'},
+        ],
+        noindex: nil,
+        canonical: request.original_url, # 優先されるurl
+        charset: 'UTF-8',
+        og: {
+            title: :title,
+            description: :description,
+            type: 'website',
+            url: request.original_url,
+            image: image_tag('/logo.png'),
+            site_name: 'Unfowld',
+            locale: 'ja_JP'
+        },
+        twitter: {
+            site: '@ツイッターのアカウント名',
+            card: 'summary',
+            image: image_tag('/logo.png') # ツイッター専用のイメージ
+        }
+    }
+  end
 end
