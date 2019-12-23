f3c9b42a764929cebf313258f81b2bc35b530b35
kaji-hiro <kajihiro190206@gmail.com>
2019-12-23 18:31:44 +0900

    fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -29,7 +29,7 @@ module ApplicationHelper
   end
 
   def notify_slack(text)
-    message = Slack::Notifier.new 'https://hooks.slack.com/services/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'
+    message = Slack::Notifier.new 'https://hooks.slack.com/services/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'
     message.ping(':speaker:ユーザーからのお問い合わせです')
     message.ping('```' + text + '```')
   end
