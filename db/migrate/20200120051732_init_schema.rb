e40018480c66385fe673d61c90257dca196c8f6e
kaji-hiro <kajihiro190206@gmail.com>
2020-02-09 12:38:47 +0900

    fix database

--- db/migrate/20200120051732_init_schema.rb
+++ db/migrate/20200120051732_init_schema.rb
@@ -1,6 +1,6 @@
 class InitSchema < ActiveRecord::Migration[6.0]
   def up
-    create_table 'action_text_rich_texts', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci' do |t|
+    create_table 'action_text_rich_texts', options: 'CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci' do |t|
       t.string 'name', null: false
       t.text 'body', size: :long
       t.string 'record_type', null: false
@@ -9,7 +9,7 @@ class InitSchema < ActiveRecord::Migration[6.0]
       t.datetime 'updated_at', precision: 6, null: false
       t.index %w[record_type record_id name], name: 'index_action_text_rich_texts_uniqueness', unique: true
     end
-    create_table 'active_storage_attachments', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci' do |t|
+    create_table 'active_storage_attachments', options: 'CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci' do |t|
       t.string 'name', null: false
       t.string 'record_type', null: false
       t.bigint 'record_id', null: false
@@ -18,7 +18,7 @@ class InitSchema < ActiveRecord::Migration[6.0]
       t.index ['blob_id'], name: 'index_active_storage_attachments_on_blob_id'
       t.index %w[record_type record_id name blob_id], name: 'index_active_storage_attachments_uniqueness', unique: true
     end
-    create_table 'active_storage_blobs', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci' do |t|
+    create_table 'active_storage_blobs', options: 'CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci' do |t|
       t.string 'key', null: false
       t.string 'filename', null: false
       t.string 'content_type'
@@ -28,7 +28,7 @@ class InitSchema < ActiveRecord::Migration[6.0]
       t.datetime 'created_at', null: false
       t.index ['key'], name: 'index_active_storage_blobs_on_key', unique: true
     end
-    create_table 'articles', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci' do |t|
+    create_table 'articles', options: 'CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci' do |t|
       t.string 'title'
       t.text 'content'
       t.bigint 'user_id', null: false
@@ -37,7 +37,7 @@ class InitSchema < ActiveRecord::Migration[6.0]
       t.integer 'article_type', null: false
       t.index ['user_id'], name: 'index_articles_on_user_id'
     end
-    create_table 'users', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci' do |t|
+    create_table 'users', options: 'CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci' do |t|
       t.string 'name'
       t.string 'email'
       t.string 'password_digest'
